b557df94b9437e17a9f54e018a4cd17d9e604805
Deniel Salvacion <denielsalvacion@gmail.com>
2021-02-06 17:22:06 +0800

    updated css

--- src/project-data.js
+++ src/project-data.js
@@ -5,31 +5,31 @@ const dataProject = [
         key: 1,
         imgUrl: 'https://denyyel914.github.io/Movie-list/',
         title: 'Movie list generator',
-        location: '/img/Movie.png'
+        location: '../Components../img/Movie.png'
     },
     {
         key: 2,
         imgUrl: 'https://denyyel914.github.io/Todo-list/',
         title: 'Todo-list',
-        location: './img/Todo-list.png'
+        location: './Components/img/Todo-list.png'
     },
     {
         key: 3,
         imgUrl: 'https://denyyel914.github.io/Movie-list/',
         title: 'Playstation Cloning',
-        location: '/img/Screenshot 2020-11-03 13.12.16.png'
+        location: '/Components/img/Screenshot 2020-11-03 13.12.16.png'
     },
     {
         key: 4,
         imgUrl: 'https://denyyel914.github.io/Playstation-Cloning/',
         title: 'Gaming Website',
-        location: 'img/Screenshot 2020-11-03 13.15.58.png'
+        location: './Components/img/ 2020-11-03 13.15.58.png'
     },
     {
         key: 5,
         imgUrl: 'https://denyyel914.github.io/',
         title: 'Xbox Cloning',
-        location: 'img/Screenshot 2020-11-03 13.05.06.png'
+        location: './Components/img/ 2020-11-03 13.05.06.png'
     }
 ]
 
